d2eb631cd2cba346f4e9bb039c37086933a7b718
Israa Diab <93504362+israa-diab@users.noreply.github.com>
2022-03-11 00:37:33 +0200

    Update settings.py
    media setting

--- prototype/settings.py
+++ prototype/settings.py
@@ -120,6 +120,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+#media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images', )
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
